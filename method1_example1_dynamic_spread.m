ab8884e2dac617724664b8e8e616eb271c901865
Grzegorz-00 <grzegorz00new@gmail.com>
2017-06-26 09:54:49 +0200

    Presentation

--- method1_example1_dynamic_spread.m
+++ method1_example1_dynamic_spread.m
@@ -23,6 +23,7 @@ points = zeros(8, maxTime);
 targets = zeros(1, maxTime);
 results = zeros(1, maxTime);
 errorArray = zeros(2, 0);
+spreadArray = zeros(2, 0);
 
 errorPoints = zeros(2, 0);
 
@@ -59,6 +60,7 @@ for i = step:step:maxTime-step
     errorVector = abs(results(1:step+1) - targets(i:step+i));
     errorArray = [errorArray, [i; sum(errorVector)*100/step]];
     
+    
     errorPoints = zeros(8, 0);
     for j = i:step+i
         if results(j-i+1) ~= targets(j)
@@ -75,20 +77,27 @@ for i = step:step:maxTime-step
        end
     end
     spread = calculateSpread( points(:,i:step+i-1), targets(:, i:step+i-1), c_it-1, points(:,i:step+i-1), targets(:, i:step+i-1), c_it-1 );
+    spreadArray = [spreadArray, [i; spread]];
     net = newpnn(points(:,i:step+i), T, spread);
 
     
-    subplot(2,1,1);
+    subplot(3,1,1);
     lim = 10;
  
     plot(points(1,i:2:step+i), points(2,i:2:step+i), 'r*', points(1,i+1:2:step+i), points(2,i+1:2:step+i), 'g*', errorPoints(1,:), errorPoints(2,:), 'b*');
     xlim([-lim,lim]);
     ylim([-lim,lim]);
     
-    subplot(2,1,2);
+    subplot(3,1,2);
     plot(errorArray(1,:), errorArray(2,:));
     xlabel('ilosc dodanych punktow')
     ylabel('procent blednych punktow');
+    drawnow;
+    
+    subplot(3,1,3);
+    plot(spreadArray(1,:), spreadArray(2,:));
+    xlabel('ilosc dodanych punktow')
+    ylabel('wartosc spread');
     drawnow;
 
 end
--- method1_example2_dynamic_spread.m
+++ method1_example2_dynamic_spread.m
@@ -23,6 +23,7 @@ points = zeros(8, maxTime);
 targets = zeros(1, maxTime);
 results = zeros(1, maxTime);
 errorArray = zeros(2, 0);
+spreadArray = zeros(2, 0);
 
 errorPoints = zeros(2, 0);
 
@@ -75,21 +76,27 @@ for i = 1:step:maxTime-step
        end
     end
     spread = calculateSpread( points(:,i:step+i-1), targets(:, i:step+i-1), c_it-1, points(:,i:step+i-1), targets(:, i:step+i-1), c_it-1 );
-    
+    spreadArray = [spreadArray, [i; spread]];
     net = newpnn(points(:,i:step+i), T, spread);
 
     
-    subplot(2,1,1);
+    subplot(3,1,1);
     lim = 10;
  
     plot(points(1,i:2:step+i), points(2,i:2:step+i), 'r*', points(1,i+1:2:step+i), points(2,i+1:2:step+i), 'g*', errorPoints(1,:), errorPoints(2,:), 'b*');
     xlim([-lim,lim]);
     ylim([-lim,lim]);
     
-    subplot(2,1,2);
+    subplot(3,1,2);
     plot(errorArray(1,:), errorArray(2,:));
     xlabel('ilosc dodanych punktow')
     ylabel('procent blednych punktow');
+    drawnow;
+    
+    subplot(3,1,3);
+    plot(spreadArray(1,:), spreadArray(2,:));
+    xlabel('ilosc dodanych punktow')
+    ylabel('wartosc spread');
     drawnow;
 
 end
